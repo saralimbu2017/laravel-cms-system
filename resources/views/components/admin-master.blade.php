1480200c5f9083c1cef9148e17baa53ad87287e1
saralimbu2017 <iamsebi7@gmail.com>
2022-01-03 21:37:12 +1100

    Fixing typo for css

--- resources/views/components/admin-master.blade.php
+++ resources/views/components/admin-master.blade.php
@@ -16,7 +16,7 @@
   <link href="{{asset('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i')}}" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
+  <link href="{{asset('css/sb-admin-2.css')}}" rel="stylesheet">
 
 </head>
 
@@ -332,8 +332,8 @@
         <div class="container-fluid">
 
           <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
-
+          
+          @yield('content')
         </div>
         <!-- /.container-fluid -->
 
