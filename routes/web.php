398108e49bfb7b70fc2a6bbda0cbe9388618fc98
saralimbu2017 <iamsebi7@gmail.com>
2022-01-04 22:23:45 +1100

    Adding route for post

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
+
+Route::get('/post', [App\Http\Controllers\PostController::class, 'show'])->name('post');;
