e0b3b69cd088258a960c326972115929a970fb08
saralimbu2017 <iamsebi7@gmail.com>
2022-01-05 15:18:16 +1100

    Defining the show function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     //
-    public function show(){
-        return view('blog-post');
+    public function show($id){
+        dd( $id);
+        
+        //return view('blog-post', ['post'=>$post]);
     }
 
 }
