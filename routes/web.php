0335c1c75e1827d397653d8ce4db334fcfed27ba
saralimbu2017 <iamsebi7@gmail.com>
2022-01-05 15:36:08 +1100

    Adding auth middlware

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,9 @@ Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
-
 Route::get('/post/{post}', [App\Http\Controllers\PostController::class, 'show'])->name('post');
 
+Route::middleware(['auth'])->group(function () {
+  Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
+
+});
