2fc0913d4377ff044ef51ffba5fbb83c8f593b3d
saralimbu2017 <iamsebi7@gmail.com>
2022-01-06 23:20:29 +1100

    Creating edit view

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,8 +1,9 @@
 <x-admin-master>
   @section('content')
     <h1>Edit</h1>
-        <form method="post" action="{{route('post.store')}}" enctype="multipart/form-data">
+        <form method="post" action="{{route('post.update',$post->id)}}" enctype="multipart/form-data">
           @csrf
+          @method('PATCH')
           <div class="form-group">
               <label for="title">Title</label>
                 <input type="text" name="title" class="form-control" id="title" placeholder="Enter title" value="{{$post->title}}">
@@ -13,7 +14,7 @@
               <input type="file" name="post_image" class="form-control" id="post_image">
           </div>
           <div class="form-group">
-            <textarea name="body" id="body" cols="30" rows="10" class="form-control" value="{{$post->body}}"></textarea>
+            <textarea name="body" id="body" cols="30" rows="10" class="form-control" value="{{$post->content}}"></textarea>
           </div>
 
 
