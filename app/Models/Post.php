3a2160b01c9d0e7b8aea9f25ef341a172e955c50
saralimbu2017 <iamsebi7@gmail.com>
2022-01-06 21:08:52 +1100

    Setting accessor and mutator functions

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,7 +10,18 @@ class Post extends Model
     use HasFactory;
 
     protected $guarded = [];
+
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function setPostImageAttribute($value){
+        $this->attributes['post_image'] = asset($value);
+    }
+
+    public function getPostImageAttribute($value){
+        return asset($value);
+    }
+    
+
 }
