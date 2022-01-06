21301c9c95e52becdecbd17cf6452f48a3f2c205
saralimbu2017 <iamsebi7@gmail.com>
2022-01-06 23:01:16 +1100

    Adding route for edit

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,8 @@ Route::middleware(['auth'])->group(function () {
 
   Route::post('/admin/posts', [App\Http\Controllers\PostController::class, 'store'])->name('post.store');
 
+  Route::get('/admin/posts/{post}/edit', [App\Http\Controllers\PostController::class, 'edit'])->name('post.edit');
+
   Route::delete('/admin/posts/{post}/destroy', [App\Http\Controllers\PostController::class, 'destroy'])->name('post.destroy');
 
 });
