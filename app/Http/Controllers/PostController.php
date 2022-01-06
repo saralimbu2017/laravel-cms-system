1039dbe2d740c50050fc4175f59a4ebc177440ca
saralimbu2017 <iamsebi7@gmail.com>
2022-01-06 23:19:57 +1100

    Defining update function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -37,7 +37,7 @@ class PostController extends Controller
         
         //dd($request->input('post_image'));
         auth()->user()->posts()->create($inputs);
-        // Session::flash('post-created-message','Post was created'.$inputs['title']);
+        Session::flash('post-created-message','Post was created'.$inputs['title']);
         return redirect()->route('post.index');
 
     }
@@ -49,6 +49,26 @@ class PostController extends Controller
     public function destroy(Post $post){
         $post->delete();
         Session::flash('message','Post was deleted');
+        return back();
+    }
+
+    public function update(Post $post){
+        $inputs = request()->validate([
+            'title'=>'required|min:8|max:255',
+            'post_image'=>'file',
+            'body'=>'required'
+        ]);
+
+        if(request('post_image')){
+            $inputs['post_image']=request('post_image')->store('images');
+            $post->post_image = $inputs['post_image'];
+        }
+
+        $post->title = $inputs['title'];
+        $post->body = $inputs['body'];
+        //dd($request->input('post_image'));
+        auth()->user()->posts()->save($post);
+        Session::flash('post-updated-message','Post was updated'.$inputs['title']);
         return back();
     }
 }
