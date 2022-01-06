0ef6144d34c430954b5fbd779b4d61a51027f62d
saralimbu2017 <iamsebi7@gmail.com>
2022-01-06 17:12:50 +1100

    Adding post owner column

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -12,6 +12,7 @@
                   <thead>
                     <tr>
                       <th>Id</th>
+                      <th>Owner</th>
                       <th>Title</th>
                       <th>Image</th>
                       <th>Created At</th>
@@ -21,6 +22,7 @@
                   <tfoot>
                     <tr>
                       <th>Id</th>
+                      <th>Owner</th>
                       <th>Title</th>
                       <th>Image</th>
                       <th>Created At</th>
@@ -31,6 +33,7 @@
                     @foreach($posts as $post)
                       <tr>
                         <td>{{$post->id }}</td>
+                        <td>{{$post->user->name }}</td>
                         <td>{{$post->title }}</td>
                         <td>
                           <img height="40px" src="{{$post->post_image }}" alt="">
