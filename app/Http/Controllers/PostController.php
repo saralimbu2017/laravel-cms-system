89fd85c3f1451b7afa6a5b3c1683ef0bd17959f2
saralimbu2017 <iamsebi7@gmail.com>
2022-01-07 00:11:42 +1100

    Updating edit function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -46,6 +46,7 @@ class PostController extends Controller
     }
 
     public function edit(Post $post){
+        $this->authorize('view',$post);
         return view('admin.posts.edit', ['post'=>$post]);
     }
 
