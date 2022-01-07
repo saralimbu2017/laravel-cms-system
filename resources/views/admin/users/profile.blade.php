dc3e271bdfa8386b95d5ec363a0eb6715057921f
saralimbu2017 <iamsebi7@gmail.com>
2022-01-08 06:53:26 +1100

    Adding user profile form

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -1,7 +1,32 @@
 <x-admin-master>
   @section('content')
     <h1>User Profile for : {{$user->name}}</h1>
+      <div class="row">
+        <div class="col-sm-6">
+          <form method="post" action="" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" name="name" class="form-control" id="name"  value="{{$user->name}}">
+            </div>
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="text" name="email" class="form-control" id="email" value="{{$user->email}}">
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" name="password" class="form-control" id="password" >
+            </div>
+            <div class="form-group">
+              <label for="password-confirm">Confirm Password</label>
+              <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" >
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            
 
+          </form>
+        </div>
+      </div>
   @endsection
 
 </x-admin-master>
