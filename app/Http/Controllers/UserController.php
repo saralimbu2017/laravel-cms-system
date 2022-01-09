09df8200d0a6821520444d07eaaa1df1ea949627
saralimbu2017 <iamsebi7@gmail.com>
2022-01-09 22:16:27 +1100

    Importing User model and fixing typo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     //
     public function show(User $user){
-        return view('admin.users.profile',['user'=>$users]);
+        return view('admin.users.profile',['user'=>$user]);
     }
 }
