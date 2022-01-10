af34ddc651720221b97f60e2ff5dbc1d0078ec57
saralimbu2017 <iamsebi7@gmail.com>
2022-01-10 13:16:10 +1100

    Adding username to the database

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,11 +65,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        //return "hello, I am at create function.";
         return User::create([
             'username' => $data['username'],
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => $data['password'],
+            'password' => $data['password']
         ]);
     }
 }
