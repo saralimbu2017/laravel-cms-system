d613a63f41e7897fb0b21108c0f7ff01f46ceafd
saralimbu2017 <iamsebi7@gmail.com>
2022-01-11 12:21:56 +1100

    Defining update method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,4 +11,10 @@ class UserController extends Controller
     public function show(User $user){
         return view('admin.users.profile',['user'=>$user]);
     }
+
+    public function update(User $user){
+        if(request('avatar')){
+            dd(request('avatar'));
+        }
+    }
 }
