1a2bf7f046dd0b90c0e3148a1c7c416309b6de3c
saralimbu2017 <iamsebi7@gmail.com>
2022-01-12 16:32:35 +1100

    Adding table

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,5 +3,59 @@
 
     <h1>Users</h1>
 
+    <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="users-table" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>Id</th>
+                      <th>Owner</th>
+                      <th>Title</th>
+                      <th>Image</th>
+                      <th>Created At</th>
+                      <th>Updated At</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <tfoot>
+                    <tr>
+                      <th>Id</th>
+                      <th>Owner</th>
+                      <th>Title</th>
+                      <th>Image</th>
+                      <th>Created At</th>
+                      <th>Updated At</th>
+                      <th>Delete</th>
+                    </tr>
+                  </tfoot>
+                  <tbody>
+                    
+                  </tbody>
+                 
+                </table>
+              </div>
+            </div>
+          </div>
+          <div class="dflex">
+            <div class="mx-auto">
+             
+            </div>
+          </div>
+
+  @endsection
+
+
+  @section('scripts')
+    <!-- Page level plugins -->
+    <script src="{{asset('vendor/datatables/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{asset('js/demo/datatables-demo.js')}}"></script>
+    
   @endsection
 </x-admin-master>
