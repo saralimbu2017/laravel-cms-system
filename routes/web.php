ce329cee0c73c8fcbcf128a58d583c18fd37f0f1
saralimbu2017 <iamsebi7@gmail.com>
2022-01-13 17:25:12 +1100

    Adding user profile route to role:Admin middleware

--- routes/web.php
+++ routes/web.php
@@ -40,8 +40,7 @@ Route::middleware(['auth'])->group(function () {
 
   Route::patch('/admin/posts/{post}/update', [App\Http\Controllers\PostController::class, 'update'])->name('post.update');
 
-  Route::get('/admin/users/{user}/profile', [App\Http\Controllers\UserController::class, 'show'])->name('user.profile.show');
-
+ 
   Route::put('/admin/users/{user}/update', [App\Http\Controllers\UserController::class, 'update'])->name('user.profile.update');
 
  
@@ -51,4 +50,6 @@ Route::middleware(['auth'])->group(function () {
 
 Route::middleware(['role:ADMIN'])->group(function () {
   Route::get('/admin/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+
+  Route::get('/admin/users/{user}/profile', [App\Http\Controllers\UserController::class, 'show'])->name('user.profile.show');
 });
