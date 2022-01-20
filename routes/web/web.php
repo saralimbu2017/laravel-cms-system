9f082bff72bac95a00ac9890f9e493dd9eccab6b
saralimbu2017 <iamsebi7@gmail.com>
2022-01-20 23:17:33 +1100

    Definging route for detach

--- routes/web/web.php
+++ routes/web/web.php
@@ -55,4 +55,6 @@ Route::middleware(['can:view,user'])->group(function(){
   Route::get('admin/users/{user}/profile',[App\Http\Controllers\UserController::class, 'show'])->name('user.show');
 });
 
-Route::put('/users/{role}/attach',[App\Http\Controllers\UserController::class, 'attach'])->name('user.attach');
+Route::put('/users/{user}/attach', [App\Http\Controllers\UserController::class, 'attach'])->name('user.role.attach');
+
+Route::put('/users/{user}/detach', [App\Http\Controllers\UserController::class, 'detach'])->name('user.role.detach');
