f2b7851e78e672e1c7cf0fd73e375805a0e4a821
saralimbu2017 <iamsebi7@gmail.com>
2022-01-20 23:16:26 +1100

    Defining detach method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -38,8 +38,14 @@ class UserController extends Controller
     }
 
     public function attach(User $user){
-        
+
         $user->roles()->attach(request('role'));
+        return back();
+    } 
+
+    public function detach(User $user){
+        
+        $user->roles()->detach(request('role'));
         return back();
     } 
 
