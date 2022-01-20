3f89aa556bdde3417eeab792816ede60fde5fd3e
saralimbu2017 <iamsebi7@gmail.com>
2022-01-21 10:30:53 +1100

    Updating the hyperlinks

--- resources/views/components/admin/sidebar/admin-sidebar-users-links.blade.php
+++ resources/views/components/admin/sidebar/admin-sidebar-users-links.blade.php
@@ -6,7 +6,7 @@
   <div id="collapseUsers" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
       <h6 class="collapse-header">Users</h6>
-      <a class="collapse-item" href="">Create a Post</a>
+      <a class="collapse-item" href="{{route('post.create')}}">Create a Post</a>
       <a class="collapse-item" href="{{route('users.index')}}">View All Users</a>
     </div>
   </div>
