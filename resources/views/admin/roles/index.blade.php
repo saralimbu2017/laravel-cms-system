b0c6712d6056026d4059a3d47c0a2720f7745f33
saralimbu2017 <iamsebi7@gmail.com>
2022-01-21 13:23:00 +1100

    Displaying contents

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -16,39 +16,33 @@
           <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
             <thead>
               <tr>
-                <th>Options</th>
                 <th>Id</th>
                 <th>Name</th>
                 <th>Slug</th>
-                <th>Attach</th>
-                <th>Detach</th>
+                <!-- <th>Created At</th>
+                <th>Updated At</th> -->
               </tr>
             </thead>
             <tfoot>
               <tr>
-                <th>Options</th>
                 <th>Id</th>
                 <th>Name</th>
                 <th>Slug</th>
-                <th>Attach</th>
-                <th>Detach</th>
+                <!-- <th>Created At</th>
+                <th>Updated At</th> -->
               </tr>
             </tfoot>
             <tbody>
-              
+              @foreach($roles as $role)
               <tr>
-                <td><input type="checkbox"></td>
-                
-                
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td> 
-                <td></td>
+                <td>{{$role->id}}</td>
+                <td>{{$role->name}}</td>
+                <td>{{$role->slug}}</td>
+        
                   
                 
               </tr>
-              
+              @endforeach 
             </tbody>
           </table>
           </div>
