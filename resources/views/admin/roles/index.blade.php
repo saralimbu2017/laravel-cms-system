a95ce5972ed171d904c13abfb7c91d18f1c2196a
saralimbu2017 <iamsebi7@gmail.com>
2022-01-21 11:45:39 +1100

    Displaying table and input form

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -1,5 +1,56 @@
 <x-admin-master>
   @section('content')
-      Roles
+      <div class="row">
+          <div class="col-sm-3">
+            <form method="post" action="">
+                @csrf
+                <div class="form-group">
+                  <label for="name">Name</label>
+                  <input type="text" id="name" class="form-control">
+                </div>
+                </form>
+          </div>
+          <div class="col-sm-9">
+          <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+              <tr>
+                <th>Options</th>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Slug</th>
+                <th>Attach</th>
+                <th>Detach</th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <th>Options</th>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Slug</th>
+                <th>Attach</th>
+                <th>Detach</th>
+              </tr>
+            </tfoot>
+            <tbody>
+              
+              <tr>
+                <td><input type="checkbox"></td>
+                
+                
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td> 
+                <td></td>
+                  
+                
+              </tr>
+              
+            </tbody>
+          </table>
+          </div>
+    
+      </div>
   @endsection
 </x-admin-master>
