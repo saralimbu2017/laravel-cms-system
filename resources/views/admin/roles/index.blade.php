22fdcf651a33d9160750cf33d6e15ca26908b8d7
saralimbu2017 <iamsebi7@gmail.com>
2022-01-21 11:52:58 +1100

    Adding action page and css styles

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -2,12 +2,13 @@
   @section('content')
       <div class="row">
           <div class="col-sm-3">
-            <form method="post" action="">
+            <form method="post" action="{{route('roles.store')}}">
                 @csrf
                 <div class="form-group">
                   <label for="name">Name</label>
-                  <input type="text" id="name" class="form-control">
+                  <input type="text" id="name" name="name" class="form-control">
                 </div>
+                <button class="btn btn-primary btn-block" type="submit">Create</button>
                 </form>
           </div>
           <div class="col-sm-9">
