90be4ab93bbb7dd385ae5a6af4c6dbaacc0195b9
saralimbu2017 <iamsebi7@gmail.com>
2022-01-21 11:48:17 +1100

    Adding route

--- routes/web/roles.php
+++ routes/web/roles.php
@@ -1,5 +1,6 @@
 <?php
   Route::get('/roles',[App\Http\Controllers\RoleController::class, 'index'])->name('roles.index');
 
+  Route::post('/roles',[App\Http\Controllers\RoleController::class, 'store'])->name('roles.store');
 
 ?>
