98de629dc6342feac004fb4da824996d6ce91a45
saralimbu2017 <iamsebi7@gmail.com>
2022-01-23 22:34:40 +1100

    Adding link

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -42,7 +42,7 @@
                     @foreach($users as $user)
                     <tr>
                       <td>{{$user->id}} </td>
-                      <td>{{$user->username}} </td>
+                      <td><a href="{{route('roles.edit', $role->id)}}"></a>{{$user->username}} </td>
                       <td><img height="50px" src="{{$user->avatar}}" alt=""> </td>
                       <td>{{$user->name}} </td>
                       <td>{{ \Carbon\Carbon::parse($user->created_at)->diffForHumans() }}  </td>
