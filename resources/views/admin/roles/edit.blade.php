8bd4c1cffb98f751fbc14a399c4e72947a559f5a
saralimbu2017 <iamsebi7@gmail.com>
2022-01-24 11:35:10 +1100

    Creating edit form

--- resources/views/admin/roles/edit.blade.php
+++ resources/views/admin/roles/edit.blade.php
@@ -2,6 +2,17 @@
   @section('content')
 
     <h1>Edit Role: {{$role->name}}</h1>
+    <form method="post" action="{{route('roles.update', $role->id)}}">
+      @csrf
+      @method('PUT')
+      
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" value="{{$role->name}}">
+      </div>
+
+      <button class="btn btn-primary">Update</button>
+    </form>
   @endsection
 
 </x-admin-master> 
