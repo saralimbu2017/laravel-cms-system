f4ff5ac547bc087c1ca95fc4b4819ccbf4d97cb9
saralimbu2017 <iamsebi7@gmail.com>
2022-01-24 11:41:36 +1100

    Updating css style

--- resources/views/admin/roles/edit.blade.php
+++ resources/views/admin/roles/edit.blade.php
@@ -1,18 +1,19 @@
 <x-admin-master> 
   @section('content')
-
+  <div class="col-sm-6">
     <h1>Edit Role: {{$role->name}}</h1>
     <form method="post" action="{{route('roles.update', $role->id)}}">
       @csrf
       @method('PUT')
-      
+
       <div class="form-group">
         <label for="name">Name</label>
-        <input type="text" name="name" id="name" value="{{$role->name}}">
+        <input type="text" name="name" class="form-control" id="name" value="{{$role->name}}">
       </div>
 
       <button class="btn btn-primary">Update</button>
     </form>
+    </div>
   @endsection
 
 </x-admin-master> 
