4532805df1bdbe74848d2cfe4b216c14d4d0395c
saralimbu2017 <iamsebi7@gmail.com>
2022-01-25 11:37:45 +1100

    Adding route

--- routes/web/permissions.php
+++ routes/web/permissions.php
@@ -1,5 +1,6 @@
 <?php
   Route::get('/permissions',[App\Http\Controllers\PermissionController::class, 'index'])->name('permissions.index');
 
+  Route::post('/permissions',[App\Http\Controllers\PermissionController::class, 'store'])->name('permissions.store');
 
 ?>
