266ca10fcb3a850e6f537dd8152e2a2d07cf64e0
saralimbu2017 <iamsebi7@gmail.com>
2022-01-25 11:42:00 +1100

    Defining store function

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Permission;
 
 class PermissionController extends Controller
 {
     //
     public function index(){
         return view('admin.permissions.index');
+    }
+
+    public function store(){
+        request()->validate([
+            'name' => ['required']
+        ]);
+        //return view('admin.roles.index');
+        Permission::create([
+            'name' => Str::ucfirst(request('name')),
+            'slug' => Str::of(Str::lower(request('name')))->slug('-')
+        ]);
+        // dd(request('name'));
+        return back();
     }
 }
