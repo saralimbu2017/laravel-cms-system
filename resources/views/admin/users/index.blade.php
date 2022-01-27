d40276f8b35f99ac98e10e661a82f3a49f30df33
saralimbu2017 <iamsebi7@gmail.com>
2022-01-27 21:32:50 +1100

    Fixing typos

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -9,73 +9,63 @@
     @endif
 
     <div class="card shadow mb-4">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Users</h6>
-            </div>
-            <div class="card-body">
-              <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                  <thead>
-                    <tr>
-                      <th>Id</th>
-                      <th>Username</th>
-                      <th>Avatar</th>
-                      <th>Name</th>
-                      <th>Registered date</th>
-                      <th>Update profile date</th>
-                      <th>Update profile date</th>
-                      <th>Delete</th>
-                    </tr>
-                  </thead>
-                  <tfoot>
-                    <tr>
-                      <th>Id</th>
-                      <th>Username</th>
-                      <th>Avatar</th>
-                      <th>Name</th>
-                      <th>Registered date</th>
-                      <th>Update profile date</th>
-                      <th>Delete</th>
-                    </tr>
-                  </tfoot>
-                  <tbody>
-                    @foreach($users as $user)
-                    <tr>
-                      <td>{{$user->id}} </td>
-                      <td><a href="{{route('roles.edit', $role->id)}}"></a>{{$user->username}} </td>
-                      <td><img height="50px" src="{{$user->avatar}}" alt=""> </td>
-                      <td>{{$user->name}} </td>
-                      <td>{{ \Carbon\Carbon::parse($user->created_at)->diffForHumans() }}  </td>
-                      <td>{{ \Carbon\Carbon::parse($user->updated_at)->diffForHumans() }} </td>
-                      <td>
-                        <form method="POST" action="{{route('user.destroy', $user->id)}}">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-danger">Delete</button>
-                        </form>
-                      </td>
-                    </tr>
+      <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Users</h6>
+      </div>
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Username</th>
+                <th>Avatar</th>
+                <th>Name</th>
+                <th>Registered date</th>
+                <th>Update profile date</th>
+                <th>Update profile date</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <th>Id</th>
+                <th>Username</th>
+                <th>Avatar</th>
+                <th>Name</th>
+                <th>Registered date</th>
+                <th>Update profile date</th>
+                <th>Delete</th>
+              </tr>
+            </tfoot>
+            <tbody>
+              @foreach($users as $user)
+              <tr>
+                <td>{{$user->id}} </td>
+                <td><a href="{{route('roles.edit', $role->id)}}"></a>{{$user->username}} </td>
+                <td><img height="50px" src="{{$user->avatar}}" alt=""> </td>
+                <td>{{$user->name}} </td>
+                <td>{{ \Carbon\Carbon::parse($user->created_at)->diffForHumans() }}  </td>
+                <td>{{ \Carbon\Carbon::parse($user->updated_at)->diffForHumans() }} </td>
+                <td>
+                  <form method="POST" action="{{route('user.destroy', $user->id)}}">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger">Delete</button>
+                </form>
+              </td>
+            </tr>
 
-                    @endforeach
+            @endforeach
 
-                  </tbody>
+          </tbody>
                  
-                </table>
-              </div>
-            </div>
-          </div>
-          <div class="dflex">
-            <div class="mx-auto">
-             
-            </div>
-          </div>
-
-  
-  
+          </table>
+        </div>
+      </div>
+    </div>
   
   @endsection
-
-
   @section('scripts')
     <!-- Page level plugins -->
     <script src="{{asset('vendor/datatables/jquery.dataTables.min.js')}}"></script>
